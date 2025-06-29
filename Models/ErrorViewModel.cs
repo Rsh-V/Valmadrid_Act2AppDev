227bc3a9174ab902313bfa6c85c23c0344a0f362
reignetteV <irishreignette@gmail.com>
2025-06-29 20:38:31 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Valmadrid_Act2AppDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
